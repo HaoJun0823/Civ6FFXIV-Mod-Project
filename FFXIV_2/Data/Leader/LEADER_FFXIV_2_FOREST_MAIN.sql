4067102d5ac82749daf1ce7b414e15fa525e543b
HaoJun0823 <developer@haojun0823.xyz>
2021-11-04 00:27:45 +0800

    Try fix Forest Leader.(All units faith)

--- FFXIV_2/Data/Leader/LEADER_FFXIV_2_FOREST_MAIN.sql
+++ FFXIV_2/Data/Leader/LEADER_FFXIV_2_FOREST_MAIN.sql
@@ -13,14 +13,21 @@ INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES
 ('MODIFIER_FFXIV_2_UNITS_HEAL_PER_TURN', 'Type', 'ALL');
 
 INSERT INTO TraitModifiers (TraitType, ModifierId) VALUES 
+('TRAIT_LEADER_FFXIV_2_FOREST_MAIN', 'MODIFIER_FFXIV_2_CAN_BUY_UNIT_BY_FAITH_RECON');
+
+INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent, NewOnly, OwnerRequirementSetId, SubjectRequirementSetId) VALUES 
+('MODIFIER_FFXIV_2_CAN_BUY_UNIT_BY_FAITH_RECON', 'MODIFIER_PLAYER_CITIES_ENABLE_UNIT_FAITH_PURCHASE', 0, 0, 0, NULL, NULL);
+
+INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES 
+('MODIFIER_FFXIV_2_CAN_BUY_UNIT_BY_FAITH_RECON', 'Tag', 'CLASS_RECON');
+
+
+INSERT INTO TraitModifiers (TraitType, ModifierId) VALUES 
 ('TRAIT_LEADER_FFXIV_2_FOREST_MAIN', 'MODIFIER_FFXIV_2_CAN_BUY_UNIT_BY_FAITH');
 
 INSERT INTO Modifiers (ModifierId, ModifierType, RunOnce, Permanent, NewOnly, OwnerRequirementSetId, SubjectRequirementSetId) VALUES 
 ('MODIFIER_FFXIV_2_CAN_BUY_UNIT_BY_FAITH', 'MODIFIER_PLAYER_CITIES_ENABLE_UNIT_FAITH_PURCHASE', 0, 0, 0, NULL, NULL);
 
 INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES 
-('MODIFIER_FFXIV_2_CAN_BUY_UNIT_BY_FAITH', 'Tag', 'True');
-
-
-
+('MODIFIER_FFXIV_2_CAN_BUY_UNIT_BY_FAITH', 'Tag', 'CLASS_ALL_UNITS');
 
